11490fe1a75fa4c99b656112e863e429a4671791
Dylan Moberg <dylanmoberg11@gmail.com>
2016-11-09 20:36:07 -0800

    still debugging

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ let app = express()
 
 app.use( express.static(__dirname) )
 
-app.get('*', (req,res) => {
-	res.sendFile(__dirname + '/index.html')
-})
+// app.get('*', (req,res) => {
+// 	res.sendFile(__dirname + '/index.html')
+// })
 
 app.listen( PORT, () => {
 	console.log(`server started on port ${PORT}`)
