18698805f29dd72030a3e90082ac1dd4bd4d0fd4
Dylan Moberg <dylanmoberg11@gmail.com>
2016-11-09 20:41:05 -0800

    fix server

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 'use strict'
 let express = require('express')
+let path = require('path')
 const PORT = process.env.PORT || 5000
 
 let app = express()
 
 app.use( express.static(__dirname) )
 
-// app.get('*', (req,res) => {
-// 	res.sendFile(__dirname + '/index.html')
-// })
+app.get('*', (req,res) => {
+	res.sendFile(path.resolve(__dirname, 'index.html'))
+})
 
 app.listen( PORT, () => {
 	console.log(`server started on port ${PORT}`)
